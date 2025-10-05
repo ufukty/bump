817575ef19f37ec98fa76f06c8e9fb74cdd2dd0b
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-10-05 12:15:08 +0300

    int/labels: adopts tests for the parameter type change of `Increment` function

--- internal/labels/increment_test.go
+++ internal/labels/increment_test.go
@@ -1,21 +1,35 @@
 package labels
 
 import (
+	"cmp"
+	"iter"
 	"maps"
 	"slices"
 	"testing"
 )
 
+// to stablize ordering of test cases
+func sort(cases iter.Seq[Labels]) []Labels {
+	return slices.SortedFunc(cases, func(a, b Labels) int {
+		return cmp.Or(
+			cmp.Compare(a[0], b[0]),
+			cmp.Compare(a[1], b[1]),
+			cmp.Compare(a[2], b[2]),
+			cmp.Compare(a[3], b[3]),
+		)
+	})
+}
+
 func TestIncrement_Major(t *testing.T) {
-	tcs := map[string]string{
-		"v0.0.0": "v1.0.0",
-		"v0.0.1": "v1.0.0",
-		"v0.1.0": "v1.0.0",
-		"v1.0.0": "v2.0.0",
+	tcs := map[Labels]Labels{
+		{0, 0, 0}: {1, 0, 0},
+		{0, 0, 1}: {1, 0, 0},
+		{0, 1, 0}: {1, 0, 0},
+		{1, 0, 0}: {2, 0, 0},
 	}
 
-	for _, input := range slices.Sorted(maps.Keys(tcs)) {
-		t.Run(input, func(t *testing.T) {
+	for _, input := range sort(maps.Keys(tcs)) {
+		t.Run(input.String(), func(t *testing.T) {
 			expected := tcs[input]
 			got, err := Increment(input, Major)
 			if err != nil {
@@ -29,15 +43,15 @@ func TestIncrement_Major(t *testing.T) {
 }
 
 func TestIncrement_Minor(t *testing.T) {
-	tcs := map[string]string{
-		"v0.0.0": "v0.1.0",
-		"v0.0.1": "v0.1.0",
-		"v0.1.0": "v0.2.0",
-		"v1.0.0": "v1.1.0",
+	tcs := map[Labels]Labels{
+		{0, 0, 0}: {0, 1, 0},
+		{0, 0, 1}: {0, 1, 0},
+		{0, 1, 0}: {0, 2, 0},
+		{1, 0, 0}: {1, 1, 0},
 	}
 
-	for _, input := range slices.Sorted(maps.Keys(tcs)) {
-		t.Run(input, func(t *testing.T) {
+	for _, input := range sort(maps.Keys(tcs)) {
+		t.Run(input.String(), func(t *testing.T) {
 			expected := tcs[input]
 			got, err := Increment(input, Minor)
 			if err != nil {
@@ -51,15 +65,15 @@ func TestIncrement_Minor(t *testing.T) {
 }
 
 func TestIncrement_Patch(t *testing.T) {
-	tcs := map[string]string{
-		"v0.0.0": "v0.0.1",
-		"v0.0.1": "v0.0.2",
-		"v0.1.0": "v0.1.1",
-		"v1.0.0": "v1.0.1",
+	tcs := map[Labels]Labels{
+		{0, 0, 0}: {0, 0, 1},
+		{0, 0, 1}: {0, 0, 2},
+		{0, 1, 0}: {0, 1, 1},
+		{1, 0, 0}: {1, 0, 1},
 	}
 
-	for _, input := range slices.Sorted(maps.Keys(tcs)) {
-		t.Run(input, func(t *testing.T) {
+	for _, input := range sort(maps.Keys(tcs)) {
+		t.Run(input.String(), func(t *testing.T) {
 			expected := tcs[input]
 			got, err := Increment(input, Patch)
 			if err != nil {
